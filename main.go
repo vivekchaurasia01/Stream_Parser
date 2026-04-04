923b725b1288c8d77c8705bd098f4cf2a3302e90
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-04-04 17:45:04 +0530

    Some Problem  while handling error in Scanner Error handling

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"log"
+)
 
-func main(){
-	fmt.Println("hello")
+type WordCount struct {
+	word string
+	count int
+}
+
+func main() {
+	OpenFile,err := os.Open("Biodata.txt")
+	if err != nil {
+		fmt.Printf("Error: Could not find the file. %v\n",err)  // %v -->  Print Default format..
+	}
+	defer OpenFile.Close()
+
+	StoreData := make(map[string]int)
+
+	Scanner := bufio.NewScanner(OpenFile)  // Creates a reader configured to split input into line..
+	for Scanner.Scan() {   //Scan reads the next line (token),Returns:true → successfully read something,false → EOF or error
+		line := Scanner.Text() // Returns the line that was just read by Scan(),Does not read anything
+		fmt.Println(line)
+	}
+	if err := Scanner.Err(); err != nil {
+    log.Fatal(err)
+	}
+
+	
 }
